5df5305a59726d5af1f28ebc61f56521e6bde544
daniela <danyoro1894@gmail.com>
2024-10-01 16:28:36 -0400

    Agregado de la clase habitacion

---
+++ habitacion.java
@@ -0,0 +1,41 @@
+package hospital;
+
+public class habitacion {
+	
+	private int numero;
+	private int piso;
+	private int cant_camas;
+	private String tipo;
+	
+	public habitacion() {	}
+
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getPiso() {
+		return piso;
+	}
+	public void setPiso(int piso) {
+		this.piso = piso;
+	}
+
+	public int getCant_camas() {
+		return cant_camas;
+	}
+	public void setCant_camas(int cant_camas) {
+		this.cant_camas = cant_camas;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	
+}
