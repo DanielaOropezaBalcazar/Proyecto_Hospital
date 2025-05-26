f1d5dc4927ca3dbbe1ff75d45821caf05f521d12
judithromeroo <142543566+judithromeroo@users.noreply.github.com>
2025-05-25 22:31:57 -0400

    Add files via upload

---
+++ src/Clases/atencion.java
@@ -0,0 +1,38 @@
+package hospital;
+
+public class atencion {
+    private String detalle;
+    private String paciente;
+    private String nivelUrgencia;
+
+    public atencion(String detalle, String paciente, String nivelUrgencia) {
+        this.detalle = detalle;
+        this.paciente = paciente;
+        this.nivelUrgencia = nivelUrgencia;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getNivelUrgencia() {
+        return nivelUrgencia;
+    }
+
+    public void setNivelUrgencia(String nivelUrgencia) {
+        this.nivelUrgencia = nivelUrgencia;
+    }
+}
+
